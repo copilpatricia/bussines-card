a48f81c1c7926b3895b773bdf53a73f88638b1c0
copilpatricia <copil_patricia@yahoo.com>
2023-03-10 05:19:48 +0200

    Updates

---
+++ precache-manifest.77ad8262442c2eda80910479f71b0ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64c37b94b739fd9b499a",
+    "url": "/bussines-card/static/css/main.db2a8274.chunk.css"
+  },
+  {
+    "revision": "64c37b94b739fd9b499a",
+    "url": "/bussines-card/static/js/main.64c37b94.chunk.js"
+  },
+  {
+    "revision": "dfa14c7c75100250ae04",
+    "url": "/bussines-card/static/js/1.dfa14c7c.chunk.js"
+  },
+  {
+    "revision": "c22cf744caa32a31b775",
+    "url": "/bussines-card/static/js/runtime~main.c22cf744.js"
+  },
+  {
+    "revision": "7118f73bce8c755a143f64386b840314",
+    "url": "/bussines-card/index.html"
+  }
+];
